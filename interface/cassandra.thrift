e1e2dfbb5e7c911342ea116be1160d1e84d1ffee
jellis <jellis@cassandra1.(none)>
2008-12-12 02:37:02 +0000

    fix interfaces (ts -> 64bit; add missing files)

--- interface/cassandra.thrift
+++ interface/cassandra.thrift
@@ -1,15 +1,17 @@
-#!/usr/local/bin/thrift --java --php
-
 #
-# Interface definition for peer storage
+# Interface definition for Cassandra
 # 
 
-include "/home/pmalik/code/fbcode/fb303/if/fb303.thrift"
+include "fb303.thrift"
 
-namespace java  com.facebook.infrastructure.service
+namespace java com.facebook.infrastructure.service
+namespace py com.facebook.infrastructure.service
+namespace cpp com.facebook.infrastructure.service
+namespace php com.facebook.infrastructure.service
 
 
-struct column_t {
+struct column_t 
+{
    1: string                        columnName,
    2: string                        value,
    3: i64                           timestamp,
@@ -17,13 +19,15 @@ struct column_t {
 
 typedef map< string, list<column_t>  > column_family_map
 
-struct batch_mutation_t {
+struct batch_mutation_t 
+{
    1: string                        table,
    2: string                        key,
    3: column_family_map             cfmap,
 }
 
-struct superColumn_t {
+struct superColumn_t 
+{
    1: string                        name,
    2: list<column_t>				columns,
 }
@@ -37,20 +41,30 @@ struct batch_mutation_super_t {
 }
 
 
-service Cassandra extends fb303.FacebookService {
+service Cassandra extends fb303.FacebookService 
+{
+  list<column_t>	get_slice(string tablename,string key,string columnFamily_column, i32 start = -1 , i32 count = -1),
 
-  list<column_t> get_slice(string tablename,string key,string columnFamily_column, i32 start = -1 , i32 count = -1),
-  column_t       get_column(string tablename,string key,string columnFamily_column),
-  i32            get_column_count(string tablename,string key,string columnFamily_column),
-  async void     insert(string tablename,string key,string columnFamily_column, string cellData,i64 timestamp),
-  async void     batch_insert(batch_mutation_t batchMutation),
-  bool           batch_insert_blocking(batch_mutation_t batchMutation),
-  async void     remove(string tablename,string key,string columnFamily_column),
-  list<column_t> get_columns_since(string tablename, string key, string columnFamily_column, i64 timeStamp),
+  column_t       	get_column(string tablename,string key,string columnFamily_column),
 
-  list<superColumn_t> get_slice_super(string tablename, string key, string columnFamily_superColumnName, i32 start = -1 , i32 count = -1),
-  superColumn_t       get_superColumn(string tablename,string key,string columnFamily),
-  async void          batch_insert_superColumn(batch_mutation_super_t batchMutationSuper),
-  bool                batch_insert_superColumn_blocking(batch_mutation_super_t batchMutationSuper),
+  i32            	get_column_count(string tablename,string key,string columnFamily_column),
+
+  async void     	insert(string tablename,string key,string columnFamily_column, string cellData, i64 timestamp),
+
+  async void     	batch_insert(batch_mutation_t batchMutation),
+
+  bool           	batch_insert_blocking(batch_mutation_t batchMutation),
+
+  async void     	remove(string tablename,string key,string columnFamily_column),
+
+  list<superColumn_t> 	get_slice_super(string tablename, string key, string columnFamily_superColumnName, i32 start = -1 , i32 count = -1),
+
+  superColumn_t       	get_superColumn(string tablename,string key,string columnFamily_superColumnName),
+
+  async void          	batch_insert_superColumn(batch_mutation_super_t batchMutationSuper),
+
+  bool                	batch_insert_superColumn_blocking(batch_mutation_super_t batchMutationSuper),
 }
+
+
 
