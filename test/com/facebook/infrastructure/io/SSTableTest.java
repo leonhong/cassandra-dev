c067bec1150dcc635fad978297b0847d15b77f20
Jonathan Ellis <jonathan@office98-44.sat.rackspace.com>
2009-01-28 09:13:34 -0600

    randomize key lookups in test; split CF testing to own class

--- test/com/facebook/infrastructure/io/SSTableTest.java
+++ test/com/facebook/infrastructure/io/SSTableTest.java
@@ -1,86 +1,69 @@
 package com.facebook.infrastructure.io;
 
+import com.facebook.infrastructure.ServerTest;
+import com.facebook.infrastructure.utils.BloomFilter;
 import org.testng.annotations.Test;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Random;
-import java.util.Arrays;
-
-import com.facebook.infrastructure.utils.BloomFilter;
-import com.facebook.infrastructure.db.ColumnFamily;
-import com.facebook.infrastructure.ServerTest;
+import java.util.*;
 
 public class SSTableTest extends ServerTest {
-    // TODO test table/CFs that are actually in the storage conf.  this breaks the current test when Name indexing is on, meaning this is a crappy test.
     @Test
-    public void testOne() throws IOException {
+    public void testSingleWrite() throws IOException {
         File f = File.createTempFile("sstable", "");
-        ColumnFamily cf;
         SSTable ssTable;
 
         // write test data
         ssTable = new SSTable(f.getParent(), f.getName());
         BloomFilter bf = new BloomFilter(1000, 8);
         Random random = new Random();
-        byte[] bytes = new byte[1024*1024];
+        byte[] bytes = new byte[1024];
         random.nextBytes(bytes);
 
         String key = Integer.toString(1);
-        cf = new ColumnFamily("Test", "Standard");
-        cf.createColumn("C", bytes, 1);
-        DataOutputBuffer bufOut = new DataOutputBuffer();
-        ColumnFamily.serializerWithIndexes().serialize(cf, bufOut);
-        ssTable.append(key, bufOut);
+        ssTable.append(key, bytes);
         bf.add(key);
-
         ssTable.close(bf);
 
         // verify
         ssTable = new SSTable(f.getPath() + "-Data.db");
         DataInputBuffer bufIn = ssTable.next(key, "Test:C");
-        cf = ColumnFamily.serializer().deserialize(bufIn);
-        assert cf != null;
-        assert cf.name().equals("Test");
-        assert cf.getAllColumns().size() == 1;
-        assert Arrays.equals(cf.getColumn("C").value(), bytes);
+        byte[] bytes2 = new byte[1024];
+        bufIn.readFully(bytes2);
+        assert Arrays.equals(bytes2, bytes);
     }
 
     @Test
-    public void testTwo() throws IOException {
+    public void testManyWrites() throws IOException {
         File f = File.createTempFile("sstable", "");
-        ColumnFamily cf;
         SSTable ssTable;
+
+        TreeMap<String, byte[]> map = new TreeMap<String,byte[]>();
+        for ( int i = 100; i < 1000; ++i )
+        {
+            map.put(Integer.toString(i), ("Avinash Lakshman is a good man: " + i).getBytes());
+        }
 
         // write
         ssTable = new SSTable(f.getParent(), f.getName());
-        DataOutputBuffer bufOut = new DataOutputBuffer();
         BloomFilter bf = new BloomFilter(1000, 8);
-        for ( int i = 100; i < 1000; ++i )
+        for (String key: map.navigableKeySet())
         {
-            String key = Integer.toString(i);
-            cf = new ColumnFamily("Test", "Standard");
-            bufOut.reset();
-            cf.createColumn("C", ("Avinash Lakshman is a good man: " + i).getBytes(), i);
-            ColumnFamily.serializerWithIndexes().serialize(cf, bufOut);
-            ssTable.append(key, bufOut);
-            bf.add(key);
+            ssTable.append(key, map.get(key));
         }
         ssTable.close(bf);
 
         // verify
+        List<String> keys = new ArrayList(map.keySet());
+        Collections.shuffle(keys);
         ssTable = new SSTable(f.getPath() + "-Data.db");
-        for ( int i = 100; i < 1000; ++i )
+        for (String key: keys)
         {
-            String key = Integer.toString(i);
-
             DataInputBuffer bufIn = ssTable.next(key, "Test:C");
-            cf = ColumnFamily.serializer().deserialize(bufIn);
-            assert cf != null;
-            assert cf.name().equals("Test");
-            assert cf.getAllColumns().size() == 1;
-            assert Arrays.equals(cf.getColumn("C").value(), ("Avinash Lakshman is a good man: " + i).getBytes());
-            assert cf.getColumn("C").timestamp() == i;
+            byte[] bytes2 = new byte[map.get(key).length];
+            bufIn.readFully(bytes2);
+            assert Arrays.equals(bytes2, map.get(key));
         }
     }
 }
