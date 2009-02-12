leonhong/cassandra-dev

commit 5c4ae733101f717688976246be5be4dabef3b9cd

clean up bloomcalculation, tests.  (i thought there was a bug with computeBestK but apparently not.)

src/com/facebook/infrastructure/utils/BloomCalculations.java
@@ -52,25 +52,19 @@ public class BloomCalculations {
         {1.0, 0.393,  0.400},
         {1.0, 0.283,  0.237,  0.253},
         {1.0, 0.221,  0.155,  0.147,   0.160},
-        {1.0, 0.181,  0.109,  0.092,   0.092,   0.101},
+        {1.0, 0.181,  0.109,  0.092,   0.092,   0.101}, // 5
         {1.0, 0.154,  0.0804, 0.0609,  0.0561,  0.0578,  0.0638},
         {1.0, 0.133,  0.0618, 0.0423,  0.0359,  0.0347,  0.0364},
         {1.0, 0.118,  0.0489, 0.0306,  0.024,   0.0217,  0.0216,  0.0229},
-        {1.0, 0.105,  0.0397, 0.0228,  0.0166,  0.0141,  0.0133,  0.0135,  0.0145},
+        {1.0, 0.105,  0.0397, 0.0228,  0.0166,  0.0141,  0.0133,  0.0135,  0.0145}, // 9
         {1.0, 0.0952, 0.0329, 0.0174,  0.0118,  0.00943, 0.00844, 0.00819, 0.00846},
         {1.0, 0.0869, 0.0276, 0.0136,  0.00864, 0.0065,  0.00552, 0.00513, 0.00509},
         {1.0, 0.08,   0.0236, 0.0108,  0.00646, 0.00459, 0.00371, 0.00329, 0.00314},
         {1.0, 0.074,  0.0203, 0.00875, 0.00492, 0.00332, 0.00255, 0.00217, 0.00199},
         {1.0, 0.0689, 0.0177, 0.00718, 0.00381, 0.00244, 0.00179, 0.00146, 0.00129},
-        {1.0, 0.0645, 0.0156, 0.00596, 0.003,   0.00183, 0.00128, 0.001,   0.000852}
+        {1.0, 0.0645, 0.0156, 0.00596, 0.003,   0.00183, 0.00128, 0.001,   0.000852} // 15
     };  // the first column is a dummy column representing K=0.
 
-    public static double getFailureRate(int bucketsPerElement){
-        int k = computeBestK(bucketsPerElement);
-        if(bucketsPerElement >= probs.length) bucketsPerElement = probs.length-1;
-        return probs[bucketsPerElement][k];
-    }
-    
     /**
      * Given the number of buckets that can be used per element, return the optimal
      * number of hash functions in order to minimize the false positive rate.
@@ -89,9 +83,14 @@ public class BloomCalculations {
      * A wrapper class that holds two key parameters for a Bloom Filter: the
      * number of hash functions used, and the number of buckets per element used.
      */
-    public static class BloomSpecification {
-        int K; // number of hash functions.
-        int bucketsPerElement;
+    public static final class BloomSpecification {
+        final int K; // number of hash functions.
+        final int bucketsPerElement;
+
+        public BloomSpecification(int k, int bucketsPerElement) {
+            K = k;
+            this.bucketsPerElement = bucketsPerElement;
+        }
     }
 
     /**
@@ -107,28 +106,26 @@ public class BloomCalculations {
      * less than specified by the function call.
      */
     public static BloomSpecification computeBucketsAndK(double maxFalsePosProb){
-        BloomSpecification spec = new BloomSpecification();
-        spec.bucketsPerElement = 2;
-        spec.K = optKPerBuckets[spec.bucketsPerElement];
-
-        // Handle the trivial cases:
-        if(maxFalsePosProb >= probs[minBuckets][minK]) return spec;
+        // Handle the trivial cases
+        if(maxFalsePosProb >= probs[minBuckets][minK]) {
+            return new BloomSpecification(2, optKPerBuckets[2]);
+        }
         if(maxFalsePosProb < probs[maxBuckets][maxK]) {
-            spec.bucketsPerElement = maxBuckets;
-            spec.K = maxK;
-            return spec;
+            return new BloomSpecification(maxK, maxBuckets);
         }
 
         // First find the minimal required number of buckets:
-        while(probs[spec.bucketsPerElement][spec.K] > maxFalsePosProb){
-            spec.bucketsPerElement++;
-            spec.K = optKPerBuckets[spec.bucketsPerElement];
+        int K = 2, bucketsPerElement = optKPerBuckets[2];
+        while(probs[bucketsPerElement][K] > maxFalsePosProb){
+            bucketsPerElement++;
+            K = optKPerBuckets[bucketsPerElement];
         }
         // Now that the number of buckets is sufficient, see if we can relax K
         // without losing too much precision.
-        while(probs[spec.bucketsPerElement][spec.K-1] <= maxFalsePosProb){
-            spec.K--;
+        while(probs[bucketsPerElement][K - 1] <= maxFalsePosProb){
+            K--;
         }
-        return spec;
+
+        return new BloomSpecification(K, bucketsPerElement);
     }
 }

test/com/facebook/infrastructure/utils/BloomCalculationsTest.java
@@ -1,0 +1,18 @@
+package com.facebook.infrastructure.utils;
+
+import org.testng.annotations.Test;
+
+public class BloomCalculationsTest {
+    @Test
+    public void testComputeBestK() {
+        BloomCalculations.BloomSpecification spec = BloomCalculations.computeBucketsAndK(0.0001);
+        assert BloomCalculations.computeBestK(spec.bucketsPerElement) == spec.K;
+    }
+
+    @Test
+    public void testComputeBucketsAndK() {
+        BloomCalculations.BloomSpecification spec = BloomCalculations.computeBucketsAndK(0.0001);
+        assert spec.bucketsPerElement == 15;
+        assert spec.K == 8;
+    }
+}

test/com/facebook/infrastructure/utils/BloomFilterTest.java
@@ -7,12 +7,12 @@ import java.io.IOException;
 
 public class BloomFilterTest {
     public BloomFilter bf;
-    public BloomCalculations.BloomSpecification spec = BloomCalculations.computeBucketsAndK(0.1);
+    public BloomCalculations.BloomSpecification spec = BloomCalculations.computeBucketsAndK(0.0001);
     static final int ELEMENTS = 10000;
 
     public BloomFilterTest() {
-        spec.bucketsPerElement = 8;
         bf = new BloomFilter(ELEMENTS, spec.bucketsPerElement);
+        assert bf != null;
     }
 
     @BeforeMethod
@@ -70,11 +70,13 @@ public class BloomFilterTest {
     }
 
     @Test
-    // run with -server -mx1G
     public void timeit() {
-        bf = new BloomFilter(300 * FilterTest.ELEMENTS, FilterTest.spec.bucketsPerElement);
-        for (int i = 0; i < 100; i++) {
-            FilterTest.testFalsePositives(bf, FilterTest.getRandomKeys(new Random(), 300 * FilterTest.ELEMENTS), FilterTest.getRandomKeys(new Random(), 300 * FilterTest.ELEMENTS));
+        int size = 300 * FilterTest.ELEMENTS;
+        bf = new BloomFilter(size, FilterTest.spec.bucketsPerElement);
+        for (int i = 0; i < 10; i++) {
+            FilterTest.testFalsePositives(bf,
+                                          new KeyGenerator.RandomStringGenerator(new Random().nextInt(), size),
+                                          new KeyGenerator.RandomStringGenerator(new Random().nextInt(), size));
             bf.clear();
         }
     }

test/com/facebook/infrastructure/utils/FilterTest.java
@@ -27,17 +27,14 @@ public class FilterTest {
     }
 
     @Test
-    public void testManyInt() {
-        testManyHashes(intKeys());
-    }
-    @Test
     public void testManyRandom() {
         testManyHashes(randomKeys());
     }
 
     // used by filter subclass tests
-    
-    public static final BloomCalculations.BloomSpecification spec = BloomCalculations.computeBucketsAndK(0.1);
+
+    static final double MAX_FAILURE_RATE = 0.1;
+    public static final BloomCalculations.BloomSpecification spec = BloomCalculations.computeBucketsAndK(MAX_FAILURE_RATE);
     static final int ELEMENTS = 10000;
 
     static final ResetableIterator<String> intKeys() {
@@ -76,7 +73,7 @@ public class FilterTest {
         synchronized (System.out) {
             System.out.println("Total/this false positives: " + total_fp + "/" + fp);
         }
-        assert fp < 1.03 * keys.size() * BloomCalculations.getFailureRate(spec.bucketsPerElement);
+        assert fp < 1.01 * keys.size() * MAX_FAILURE_RATE;
     }
 
     public static Filter testSerialize(Filter f) throws IOException {

commit 513e4590af92c784957540e8e47515cc695c6fc1

fix regression caused by 5c4ae733101f717688976246be5be4dabef3b9cd

src/com/facebook/infrastructure/utils/BloomCalculations.java
@@ -115,7 +115,8 @@ public class BloomCalculations {
         }
 
         // First find the minimal required number of buckets:
-        int K = 2, bucketsPerElement = optKPerBuckets[2];
+        int bucketsPerElement = 2;
+        int K = optKPerBuckets[2];
         while(probs[bucketsPerElement][K] > maxFalsePosProb){
             bucketsPerElement++;
             K = optKPerBuckets[bucketsPerElement];